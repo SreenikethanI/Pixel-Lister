30ea3f26a5b04ed8e3ccd873e54bce32340c3ca2
Sreenikethan I <sreeni.s.iyer+github@gmail.com>
2023-09-26 10:33:53 +0400

    working on dither algos

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,18 @@
 "use strict";
 import * as DOM from './constants_dom.js';
 import * as Helper from './helper.js';
-
+import ColorThief from './color-thief.mjs';
 
 //=| TESTING |================================================================//
 DOM.loadReferences();
 DOM.TEMP_FILE_CHOOSER.addEventListener("input", async (ev) => {
     const bitmap = await Helper.loadImageFromFilePicker(DOM.TEMP_FILE_CHOOSER);
     console.log(bitmap || "load failed");
+    if (!bitmap) {return;}
+
+    const ct = new ColorThief();
+    const palette = ct.getPalette(bitmap, 20);
+    console.log(palette);
 });
 
-console.log(DOM.TEMP_FILE_CHOOSER);
 console.log("bruh");
